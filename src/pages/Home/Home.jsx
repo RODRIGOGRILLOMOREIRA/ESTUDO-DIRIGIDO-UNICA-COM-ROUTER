b60696a505386fd0a372e56ac64fb15829246613
Luiz <luiz04nl@gmail.com>
2024-11-23 14:01:15 -0300

    Adicionado Links

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Home = () => {
 
@@ -8,9 +9,36 @@ const Home = () => {
         <h1>PROJETO INTEGRADOR ÚNICA</h1>
         <h2>SISTEMA DE GESTÃO DE LOGÍSTICA</h2>
         <h3>Seja bem vindo ao SLG !</h3>
-        {/* <button>
-            ENTRAR
-        </button> */}
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/Login">Sait</Link>
+            </li>
+            <li>
+              <Link to="/Config">Config</Link>
+            </li>
+            <li>
+              <Link to="/User">User</Link>
+            </li>
+            <li>
+              <Link to="/Reset">Reset</Link>
+            </li>
+            <li>
+              <Link to="/Orders">Orders</Link>
+            </li>
+            <li>
+              <Link to="/Products">Products</Link>
+            </li>
+            <li>
+              <Link to="/Materials">Materials</Link>
+            </li>
+            <li>
+              <Link to="/Production">Production</Link>
+            </li>
+          </ul>
+        </nav>
+
       </form>
     </div>
   );
